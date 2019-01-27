46a75c2726e15a40b6348c4955fccbbe1b639296
estefania-cg1 <estefania_congote23171@elpoli.edu.co>
2019-01-27 17:03:41 -0500

    Se modifican clases de Test

--- test/com/project/ememusic/automatizacion/PageObject/Artista.java
+++ test/com/project/ememusic/automatizacion/PageObject/Artista.java
@@ -10,29 +10,74 @@ import org.openqa.selenium.WebDriver;
  */
 public class Artista {
 
-    public static void ClicArtista(WebDriver driver) {
-        driver.findElement(By.linkText("Registro de Artistas")).click();
+    WebDriver driver;
+    By selectipo = By.name("cbotipodocumento");
+    By NroDocArt = By.name("txtnumdocumento");
+    By PrimerNombre = By.name("txtprimernombre");
+    By SegundoNombre = By.name("txtsegundonombre");
+    By PrimerApellido = By.name("txtprimerapellido");
+    By SegundoApellido = By.name("txtsegundopellido");
+    By NombreArtistico = By.name("txtnombreartistico");
+    By SelectEmpresa = By.name("cboempresa");
+    By EstadoArt = By.name("optionStateS");
+    By Guardar = By.id("save");
 
+    public Artista(WebDriver driver) {
+        this.driver = driver;
     }
 
-    public static void LlenarArtista(WebDriver driver) {
-        String Artista = ("Maia");
-        driver.findElement(By.name("cbotipodocumento")).sendKeys(Keys.DOWN);
-        driver.findElement(By.name("cbotipodocumento")).sendKeys(Keys.RETURN);
-        driver.findElement(By.name("cbotipodocumento")).click();
-        driver.findElement(By.name("txtnumdocumento")).sendKeys("972367");
-        driver.findElement(By.name("txtprimernombre")).sendKeys("Monica");
-        driver.findElement(By.name("txtsegundonombre")).sendKeys("María");
-        driver.findElement(By.name("txtprimerapellido")).sendKeys("Velasquez");
-        driver.findElement(By.name("txtsegundopellido")).sendKeys("Velasquez");
-        driver.findElement(By.name("txtnombreartistico")).sendKeys("Maia");
-        driver.findElement(By.name("cboempresa")).sendKeys(Keys.DOWN);
-        driver.findElement(By.name("cboempresa")).sendKeys(Keys.RETURN);
-        driver.findElement(By.name("cboempresa")).click();
-        driver.findElement(By.name("optionStateS")).sendKeys(Keys.DOWN);
-        driver.findElement(By.name("optionStateS")).sendKeys(Keys.RETURN);
-        driver.findElement(By.name("optionStateS")).click();
-        driver.findElement(By.name("action")).click();
-        
+    public void setSelectipo(String SelectArt) {
+        driver.findElement(selectipo).sendKeys(SelectArt);
     }
+
+    public void setNroDocArt(String NroArt) {
+        driver.findElement(NroDocArt).sendKeys(NroArt);
+    }
+
+    public void setPrimerNombre(String PNombre) {
+        driver.findElement(PrimerNombre).sendKeys(PNombre);
+    }
+
+    public void setSegundoNombre(String SNombre) {
+        driver.findElement(SegundoNombre).sendKeys(SNombre);
+    }
+
+    public void setPrimerApellido(String PApellido) {
+        driver.findElement(PrimerApellido).sendKeys(PApellido);
+    }
+
+    public void setSegundoApellido(String SApellido) {
+        driver.findElement(SegundoApellido).sendKeys(SApellido);
+    }
+
+    public void setNombreArtistico(String NArtistico) {
+        driver.findElement(NombreArtistico).sendKeys(NArtistico);
+    }
+
+    public void setSelectEmpresa(String SEmpresa) {
+        driver.findElement(SelectEmpresa).sendKeys(SEmpresa);
+    }
+
+    public void setEstadoArt(String EstArt) {
+        driver.findElement(EstadoArt).sendKeys(EstArt);
+    }
+
+    public void ClicGuardarArt() {
+        driver.findElement(Guardar).click();
+    }
+
+    public void ArtistaAplication(String selectipo, String NroDocArt, String PrimerNombre, String SegundoNombre, String PrimerApellido, String SegundoApellido, String NombreArtistico, String SelectEmpresa, String EstadoArt, String Guardar) {
+    this.setSelectipo(selectipo);
+    this.setNroDocArt(NroDocArt);
+    this.setPrimerNombre(PrimerNombre);
+    this.setSegundoNombre(SegundoNombre);
+    this.setPrimerApellido(PrimerApellido);
+    this.setSegundoApellido(SegundoApellido);
+    this.setNombreArtistico(NombreArtistico);
+    this.setSelectEmpresa(SelectEmpresa);
+    this.setEstadoArt(EstadoArt);
+    this.ClicGuardarArt();
+    
+    }
+    
 }
