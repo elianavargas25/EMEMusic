4360040a303066ad3e57a6bdefdb8724dd6ce121
eliana.vargas <eliana.vargas@QXCO224.Quipux.local>
2019-01-28 18:23:57 -0500

    Se modifica tipo de dato

--- src/java/com/project/ememusic/servlet/MaestroVentas.java
+++ src/java/com/project/ememusic/servlet/MaestroVentas.java
@@ -28,7 +28,7 @@ public class MaestroVentas extends HttpServlet {
     public void limpiar() {
         venta.setIdArtista("");
         venta.setIdEmpresa("");
-        venta.setReproduccion("");
+        venta.setReproduccion(0.0);
         
     }//limpiar
 
@@ -47,7 +47,7 @@ public class MaestroVentas extends HttpServlet {
         String idArtista = request.getParameter("cboartista");
         String idEmpresa = request.getParameter("cboempresa");
         String reproduccion = request.getParameter("txtreproduccion");
-        
+        double reprod = Double.parseDouble(reproduccion);
         String mensaje = "";
         String modulo = "RegistrarVentas.jsp"; // validar con la vista
 
@@ -60,7 +60,7 @@ public class MaestroVentas extends HttpServlet {
                     try {
                         venta.setIdArtista(idArtista);
                         venta.setIdEmpresa(idEmpresa);
-                        venta.setReproduccion(reproduccion);
+                        venta.setReproduccion(reprod);
                         
                         //se guarda los datos en la tabla
                         negocio.guardarVenta(venta);
