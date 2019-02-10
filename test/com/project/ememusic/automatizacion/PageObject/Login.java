312a59815229ea1b80279ecd86219075f49f9598
estefania-cg1 <estefania_congote23171@elpoli.edu.co>
2019-02-10 16:50:36 -0500

    Se modifican casos de pruebas

--- test/com/project/ememusic/automatizacion/PageObject/Login.java
+++ test/com/project/ememusic/automatizacion/PageObject/Login.java
@@ -8,27 +8,47 @@ import org.openqa.selenium.WebDriver;
  * @author Estefanía
  */
 public class Login {
-WebDriver driver;
-By User= By.name("Usuario");
-By Password= By.name("Clave");
-By Button= By.name("Login");
 
- public Login(WebDriver driver){
+    WebDriver driver;
+    By User = By.id("Username");
+    By Password = By.id("Password");
+    By Button = By.xpath("//*[@id=\"login\"]");
+    By UserAdmin = By.xpath("/html/body/div/header/nav/ul/h4");
+    By UserOperario = By.xpath("/html/body/div/header/nav/ul/h4");
+    By Mensaje = By.xpath("/html/body/form/h1[1]");
+
+    public Login(WebDriver driver) {
         this.driver = driver;
     }
- public void setUserName(String usuario){
- driver.findElement(User).sendKeys(usuario);
- }
- public void setPass(String contraseña){
- driver.findElement(Password).sendKeys(contraseña);
- }
- 
-  public void ClicLogin (){
- driver.findElement(Button).click();
- }
-  public void LoginAplication(String User ,String Password){
-  this.setUserName(User);
-  this.setPass(Password);
-  this.ClicLogin();
-  }
+
+    public String UserOperario() {
+        return driver.findElement(UserOperario).getText();
+    }
+
+    public String USerAdmin() {
+        return driver.findElement(UserAdmin).getText();
+    }
+
+    public String Mensaje() {
+        return driver.findElement(Mensaje).getText();
+    }
+
+    public void setUserName(String usuario) {
+        driver.findElement(User).sendKeys(usuario);
+    }
+
+    public void setPass(String contraseña) {
+        driver.findElement(Password).sendKeys(contraseña);
+    }
+
+    public void ClicLogin() {
+        driver.findElement(Button).click();
+    }
+
+    public void LoginAplication(String User, String Password) {
+        this.setUserName(User);
+        this.setPass(Password);
+        this.ClicLogin();
+
+    }
 }
--- test/com/project/ememusic/automatizacion/Test/PageObjects/ArtistaTest.java
+++ test/com/project/ememusic/automatizacion/Test/PageObjects/ArtistaTest.java
@@ -1,13 +1,19 @@
-package com.project.ememusic.automatizacion.object;
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.project.ememusic.automatizacion.Test.PageObjects;
 
-import com.project.ememusic.automatizacion.PageObject.Empresa;
+import com.project.ememusic.automatizacion.PageObject.Artista;
 import com.project.ememusic.automatizacion.PageObject.Login;
 import com.project.ememusic.automatizacion.PageObject.UserPage;
 import org.junit.After;
 import org.junit.AfterClass;
-import static org.junit.Assert.assertEquals;
 import org.junit.Before;
 import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
 import org.openqa.selenium.By;
 import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
@@ -15,17 +21,15 @@ import org.openqa.selenium.chrome.ChromeDriver;
 
 /**
  *
- * @author estefanía
+ * @author Estefanía
  */
-public class EmpresaTest {
+public class ArtistaTest {
 
     private static WebDriver driver = null;
-    Empresa Empre;
     Login log;
     UserPage user;
+    Artista Art;
 
-    //Account acco;
-    //Deposit dep;
     @BeforeClass
     public static void setUpClass() {
         System.setProperty("webdriver.chrome.driver", "lib\\drivers\\chromedriver.exe");
@@ -34,7 +38,9 @@ public class EmpresaTest {
     }
 
     @AfterClass
+
     public static void tearDownClass() {
+
     }
 
     @Before
@@ -47,31 +53,41 @@ public class EmpresaTest {
         driver.quit();
     }
 
-    /**
-     * Test of loginApplication method, of class Login.
-     */
-    @org.junit.Test
-    public void EmpresaTest() {
+    @Test
+    public void ArtistaGuardar() throws InterruptedException {
         driver.findElement(By.linkText("Ingresar")).click();
         driver.findElement(By.name("Usuario")).sendKeys("admin");
         driver.findElement(By.name("Clave")).sendKeys("admin");
         driver.findElement(By.name("action")).click();
-        System.out.println("Test Empresa");
-        driver.findElement(By.linkText("Registro de Empresas")).click();
-        String Empresa = ("Music SA");
+        System.out.println("Test Artistas");
+        driver.findElement(By.linkText("Registro de Artistas")).click();
+        String Artista = ("Ricardo Arjona");
         String Mensaje = "Registro guardado correctamente";
-        driver.findElement(By.name("cbotipodocumento")).sendKeys(Keys.DOWN);
         driver.findElement(By.name("cbotipodocumento")).sendKeys(Keys.DOWN);
         driver.findElement(By.name("cbotipodocumento")).sendKeys(Keys.RETURN);
         driver.findElement(By.name("cbotipodocumento")).click();
-        driver.findElement(By.name("txtnrodocumento")).sendKeys("94584704");
-        driver.findElement(By.name("txtnombre")).sendKeys(Empresa);
-        driver.findElement(By.name("txtpagooperacion")).sendKeys("900");
+        driver.findElement(By.name("txtnumdocumento")).sendKeys("972367");
+        driver.findElement(By.name("txtprimernombre")).sendKeys("Ricardo");
+        driver.findElement(By.name("txtsegundonombre")).sendKeys("Dario");
+        driver.findElement(By.name("txtprimerapellido")).sendKeys("Arjona");
+        driver.findElement(By.name("txtsegundopellido")).sendKeys("Velasquez");
+        driver.findElement(By.name("txtnombreartistico")).sendKeys(Artista);
+        driver.findElement(By.name("cboempresa")).sendKeys(Keys.DOWN);
+        driver.findElement(By.name("cboempresa")).sendKeys(Keys.RETURN);
+        driver.findElement(By.name("cboempresa")).click();
         driver.findElement(By.name("optionStateS")).sendKeys(Keys.DOWN);
-        //driver.findElement(By.name("optionStateS")).sendKeys(Keys.RETURN);
+        driver.findElement(By.name("optionStateS")).sendKeys(Keys.RETURN);
         driver.findElement(By.name("optionStateS")).click();
         driver.findElement(By.name("action")).click();
         String MensajeMostrado = driver.findElement(By.xpath("/html/body/div/div[2]/div[2]/center/h4")).getText();
         assertEquals(Mensaje, MensajeMostrado);
+
+    }
+    
+     @Test
+    public void ArtistaBuscar () {
+    
+    
+    
     }
 }
